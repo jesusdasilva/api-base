6562ba3b830e96c1913f7dd3f6fea205115e5b72
Jesús Manuel Dasilva Barreto <jesusdasilva@gmail.com>
2024-07-18 01:40:26 -0400

    feat: Update root route message to display version 2.0

--- test/app.test.js
+++ test/app.test.js
@@ -3,8 +3,8 @@ import assert from 'node:assert';
 import request from 'supertest';
 import app from '../app.js';
 
-test('GET / should return "Hola API Base"', async () => {
+test('GET / should return "Hola API Base ver. 2.0"', async () => {
   const response = await request(app).get('/');
   assert.strictEqual(response.status, 200);
-  assert.deepStrictEqual(response.body, { message: 'Hola API Base' });
+  assert.deepStrictEqual(response.body, { message: 'Hola API Base ver. 2.0' });
 });
