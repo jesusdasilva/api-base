e646caff761b4daa912e0ad16ccf1a4fcf6a6c97
Jesús Manuel Dasilva Barreto <jesusdasilva@gmail.com>
2024-07-18 01:38:29 -0400

    chore: Update app.js to display updated message in the root route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hola API Base' });
+  res.json({ message: 'Hola API Base ver. 2.0' });
 });
 
 export default app;
